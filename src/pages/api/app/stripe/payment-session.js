47e13821eb19f35b66659b060f393570c65dbfe1
amandaahiwe <108036639+amandaahiwe@users.noreply.github.com>
2023-05-16 04:28:12 +0100

    Update payment-session.js

--- src/pages/api/app/stripe/payment-session.js
+++ src/pages/api/app/stripe/payment-session.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
     const transformedItems = req.body.cart ? req.body.cart.map((item) => ({
       quantity: 1,
       price_data: {
-        currency: 'usd',
+        currency: 'gbp',
         unit_amount: item.price * 100,
 
         product_data: {
